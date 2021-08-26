82eab420315e6db04cc2667a611aae07980fa4d6
arjansunar <arjan.gahatrajsunar@gmail.com>
2021-08-26 16:19:07 +0545

    updating partners

--- components/Home/PartnersCard.jsx
+++ components/Home/PartnersCard.jsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+// import Image from "next/image";
 import styled from "styled-components";
 
 function PartnersCard({ path }) {
   return (
     <ImgWrapper>
-      <Image height="45px" width="120px" alt="our partners" src={path} />
+      {/* <Image height="45px" width="120px" alt="our partners" src={path} /> */}
+      <img src={path} alt="our partners" />
     </ImgWrapper>
   );
 }
@@ -12,9 +14,16 @@ function PartnersCard({ path }) {
 export default PartnersCard;
 
 const ImgWrapper = styled.div`
-  max-width: 180px;
+  max-width: 230px;
   border: 1px solid #e6e6e6;
-  padding: 2.875rem 2.125rem;
+  padding: 0 1rem;
+  height: 150px;
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 100%;
+  }
 
   @media (max-width: 550px) {
     padding: 2rem 1.825rem;
